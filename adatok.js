f12dcff2aa1e692c23257153357e96bae081fe49
Raichu15678 <matefekete355@gmail.com>
2025-02-10 11:59:00 +0100

    Add files via upload

---
+++ adatok.js
@@ -0,0 +1,74 @@
+const nevekTomb=[
+    {
+        "megoldas":"Mátyás király",
+        "tipp1":"Lajos király",
+        "tipp2":"IV. Béla",
+        "tipp3":"II. András",
+    },
+    {
+        "megoldas":"Mária Terézia",
+        "tipp1":"I. Ferdinánd",
+        "tipp2":"II. József",
+        "tipp3":"I. Lipót",
+    },
+    {
+        "megoldas":"Széchenyi István",
+        "tipp1":"Kossuth Lajos",
+        "tipp2":"Deák Ferenc",
+        "tipp3":"Rákóczi Ferenc",
+    },
+    {
+        "megoldas":"Kossuth Lajos",
+        "tipp1":"Jókai Mór",
+        "tipp2":"Arany János",
+        "tipp3":"Vörösmarty Mihály",
+    },
+    {
+        "megoldas":"Winston Churchill",
+        "tipp1":"Franklin D. Roosevelt",
+        "tipp2":"Joseph Stalin",
+        "tipp3":"Dwight D. Eisenhower",
+    },
+    {
+        "megoldas":"Abraham Lincoln",
+        "tipp1":"George Washington",
+        "tipp2":"Theodore Roosevelt",
+        "tipp3":"Thomas Jefferson",
+    },
+    {
+        "megoldas":"Napóleon Bonaparte",
+        "tipp1":"Lajos Fülöp",
+        "tipp2":"XIV. Lajos",
+        "tipp3":"III. Napóleon",
+    },
+    {
+        "megoldas":"Sissi királyné",
+        "tipp1":"Mária Antónia",
+        "tipp2":"I. Erzsébet",
+        "tipp3":"Zita királyné",
+    },
+    {
+        "megoldas":"Dalai láma",
+        "tipp1":"Mahatma Gandhi",
+        "tipp2":"XI. Panchen Láma",
+        "tipp3":"Buddha",
+    },
+    {
+        "megoldas":"Deák Ferenc",
+        "tipp1":"Kossuth Lajos",
+        "tipp2":"Batthyány Lajos",
+        "tipp3":"Eötvös József",
+    },
+    {
+        "megoldas":"Nelson Mandela",
+        "tipp1":"Desmond Tutu",
+        "tipp2":"Barack Obama",
+        "tipp3":"Martin Luther King Jr.",
+    },
+    {
+        "megoldas":"Mahatma  Gandhi",
+        "tipp1":"Jawaharlal Nehru",
+        "tipp2":"Dalai Láma",
+        "tipp3":"Indira Gandhi",
+    },
+];
